66e3869d7b88da1abe0425299c9e0296af729a38
Pratiksha-Kamble <pratikshakamble2345@gmail.com>
2024-08-10 08:17:36 -0700

    DefaultValues Program

---
+++ DefaultValues.java
@@ -0,0 +1,33 @@
+package Programs_by_MKT;
+
+public class DefaultValues {
+	
+	byte by;
+	short st;
+	int i;
+	long lg;
+	char c;
+	float f;
+	double d;
+	boolean b;
+	String s;
+	
+
+	public static void main(String[] args) {
+		
+		DefaultValues dv=new DefaultValues();
+		System.out.println("Default value of byte"+dv.by);
+		System.out.println("Default value of short"+dv.st);
+		System.out.println("Default value of int"+dv.i);
+		System.out.println("Default value of long"+dv.lg);
+		System.out.println("Default value of char"+dv.c);
+		System.out.println("Default value of float"+dv.f);
+		System.out.println("Default value of double"+dv.d);
+		System.out.println("Default value of boolean"+dv.b);
+		System.out.println("Default value of String"+dv.s);
+		
+		
+
+	}
+
+}
